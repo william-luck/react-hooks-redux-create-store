0e763c5d06da4e0181ecf54827ec64de4234910d
William Luck <luck.william.h@gmail.com>
2023-03-24 10:32:42 -0300

    commit tree 91bdc9f551afc6e0185fd8a1b2b38fc5df23b829

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,25 @@
-let state;
+
+// Passing in the reducer so it can generic for any JS application
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    // This will call the reducer that is passed through. How can we pass the reducer each time? 
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state
+  }
+
+  // We've have dispatch private to the function, but still want to be able to call dispatch in response to user events, so we return an object containing the method, "the store". Why is it an object? Because we are creating the store / object? OOOOOOO OBJECT ORIENTED WITH METHOD. WE CAN CALL store.dispatch. And we can call store.getState to access the value of state.  
+  return { 
+    dispatch, 
+    getState
+  }
+}
+
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +31,20 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
+
 
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+// Returns the dispatch method in object, the object also contains all the state to be updated (let state). 
+// Passes in reducer function
+let store = createStore(reducer);
+store.dispatch({ type: "@@INIT" });
+
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
